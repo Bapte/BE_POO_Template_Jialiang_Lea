d3ea086215d96be8b0a7acbb65cc7ca8f12cf778
Bapte <jialiangbao3@gmail.com>
2024-11-27 10:18:54 +0100

    test bib dans project_name

--- project_name/project_name.ino
+++ project_name/project_name.ino
@@ -1,29 +1,30 @@
 #include "Application.h"
+#include "bibliotheque.h"
 
 Application myApplication;
 
 const int pinbutton = D7;
 const int pinsound = A0;
+
+Lightsensor lightsensor(A0);
+LED led(D1,0);
 void setup() 
 {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(pinbutton, INPUT);
-  pinMode(pinsound, INPUT);
+  lightsensor.init();
+  led.init();
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  if (digitalRead(pinbutton) == 1){
-    digitalWrite(LED_BUILTIN, LOW);
+  int light = lightsensor.getlight();
+
+  if (light < 500){
+    led.setlight(255);
   }else{
-    digitalWrite(LED_BUILTIN, HIGH);
+    led.setlight(0);
   }
 
-  if(analogRead(pinsound) > 500){
-    digitalWrite(LED_BUILTIN, LOW);
-  }else{
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
+  delay(2000);
 }
